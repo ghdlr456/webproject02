9c86945a2cc92fe9e6582f0c7bd63e620ddf263d
D7608 <D7608@DESKTOP-IQMDMLS>
2018-11-06 12:04:33 +0900

    aaaaa

---
+++ 201514800-exController2/src/main/java/kr/ac/dit/controller/StudentController.java
@@ -0,0 +1,20 @@
+package kr.ac.dit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.ac.dit.domain.StudentVO;
+
+@Controller
+
+public class StudentController {
+	@RequestMapping("read")
+	public void read(Model model) {
+		  StudentVO studentVO = new StudentVO();
+		  studentVO.setNo("201514800");
+		  studentVO.setName("Jeong ho ik");
+		  model.addAttribute(studentVO);
+		
+	}
+}
