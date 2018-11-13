ad290eb464bd396a3c4284a9f1ec93214f438656
D7608 <D7608@DESKTOP-IQMDMLS>
2018-11-13 11:26:09 +0900

    ..

--- 201514800-BoardLogon/src/main/java/kr/ac/dit/controller/MemberController.java
+++ 201514800-BoardLogon/src/main/java/kr/ac/dit/controller/MemberController.java
@@ -15,28 +15,30 @@ import kr.ac.dit.service.MemberService;
 @Controller
 @RequestMapping("/member")
 public class MemberController {
- @Autowired
- MemberService memberService;
- @RequestMapping(value = "/create", method = RequestMethod.GET)
- public void createGET() throws Exception {
- }
- 
- @RequestMapping(value = "/createPost", method = RequestMethod.POST)
- public String createPOST(MemberVO memberVO) throws Exception {
-  memberService.createMember(memberVO);
-  return "redirect:/board/list";
- }
- 
- @RequestMapping(value = "/login", method = RequestMethod.GET)
- public void loginGET(@ModelAttribute("logonDTO") LogonDTO logonDTO) throws Exception {
- }
- 
- @RequestMapping(value = "/loginPost", method = RequestMethod.POST)
- public String loginPOST(LogonDTO logonDTO, HttpSession httpSession, Model model) throws Exception {
-  MemberVO memberVO = memberService.readMember(logonDTO);
-
-  if (memberVO==null) { return "/member/login"; }
-  model.addAttribute("memberVO", memberVO);
-  return "redirect:/board/list";
- }
+	@Autowired
+	private MemberService memberService;
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public void createGET() throws Exception {
+	}
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public String createPOST(MemberVO memberVO) throws Exception {
+		memberService.createMember(memberVO);
+		return "redirect:/board/list";
+	}
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public void loginGET(@ModelAttribute("logonDTO") LogonDTO logonDTO) {
+	}
+	@RequestMapping(value = "/loginPost", method = RequestMethod.POST)
+	public void loginPOST(LogonDTO logonDTO, HttpSession httpSession, Model model) throws Exception {
+		MemberVO memberVO = memberService.readMember(logonDTO);
+		if (memberVO==null) { return; }
+		model.addAttribute("memberVO", memberVO);
+	}
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public void logout(HttpSession httpSession) {
+		if (httpSession.getAttribute("login") != null) {
+			httpSession.removeAttribute("login");
+			httpSession.invalidate();
+		}
+	}
 }
